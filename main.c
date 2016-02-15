e7c17cd0484cc921e1efa1e0e58800a090353cf4
Michael Alweis <alweism@gmail.com>
2016-02-15 15:17:09 -0500

    initial test

--- main.c
+++ main.c
@@ -3,8 +3,29 @@
 #include "sorted-list.h"
 
 int main(int argc, char** argv)
-{	void* 
-	Soretedlistpojer rgije = SLCreate(cf,df);
-	SLInsert(list, 
+{	
+	int compareInt(void* a,void* b)	
+	{	
+		int x = *(int*)a;
+		int y = *(int*)b;
+		if(x==y){return 0;}
+		if(x>y){return 1;}
+		return -1;
+	}
+
+	void destroyInt(void* a)
+	{
+		
+	}
+	int* x = malloc(sizeof(int));
+	int* y = malloc(sizeof(int));
+	*x=5;
+	*y=8;
+	int (*CompareFuncT)( void *, void * ) = &compareInt;
+	void (*DestructFuncT)( void * ) =&destroyInt;
+	SortedListPtr sl= SLCreate(CompareFuncT,DestructFuncT);
+	SLInsert(sl,x);
+	SLInsert(sl,y);
+	printf("%d",*(int*)sl->head->data);
 	return 0;
 }
